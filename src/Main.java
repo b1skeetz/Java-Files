bbf044b664914d6e015a45be9a5927546969bf70
Damir Kadyrzhanov <damirk120404@gmail.com>
2023-11-03 21:03:01 +0600

    one more time

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,51 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        List<String> messages = new ArrayList<>();
+        String buffer = "";
+        System.out.println("Start texting...");
+        while(!buffer.equals("stop")){
+            try {
+                buffer = bufferedReader.readLine().toLowerCase();
+                messages.add(buffer);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
+        WriteToFile("test.txt", messages);
+        System.out.println("-------------------");
+        ReadFromFile("test.txt");
+    }
+
+    private static void WriteToFile(String fileName, List<String> content){
+        try{
+            File file = new File(fileName);
+            PrintWriter pw = new PrintWriter(file);
+            if(file.exists()){
+                file.createNewFile();
+            }
+            for (String s : content) {
+                pw.println(s);
+            }
+            pw.close();
+        }catch(IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+    private static void ReadFromFile(String fileName) throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
     }
 }
